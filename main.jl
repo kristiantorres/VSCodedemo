b394ad9167a478cff0c5931b39eb68767f79edf5
Kristian D. T. Bautista <torresba@ualberta.ca>
2020-08-03 16:50:15 -0600

    Add initial version

---
+++ main.jl
@@ -0,0 +1,15 @@
+using DataFrames, Query, VegaDatasets, VegaLite
+
+cars = dataset("cars")
+
+cars |> @select(:Acceleration, :Name) |> collect
+
+function foo(data,origin)
+    df = data |> @filter(_.Origin==origin) |> DataFrame
+    
+    return df |> @vlplot(:point,:Acceleration,:Miles_per_Gallon)
+end
+
+p = foo(cars,"USA")
+
+p |> save("foo.png")
